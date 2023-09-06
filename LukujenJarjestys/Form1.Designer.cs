f700d22d5160534a3353532b0f33e1ea372e6c5e
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-09-06 10:39:13 +0300

    Add project files.

---
+++ LukujenJarjestys/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace LukujenJarjestys
+{
+    partial class LukujenJarjestys
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            uusiLukuTB = new TextBox();
+            VastausLB = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(46, 60);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(223, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Anna luku (-999 lopetus):";
+            // 
+            // uusiLukuTB
+            // 
+            uusiLukuTB.Location = new Point(279, 60);
+            uusiLukuTB.Margin = new Padding(5, 5, 5, 5);
+            uusiLukuTB.Name = "uusiLukuTB";
+            uusiLukuTB.Size = new Size(155, 33);
+            uusiLukuTB.TabIndex = 1;
+            uusiLukuTB.KeyPress += uusiLukuTB_KeyPress;
+            // 
+            // VastausLB
+            // 
+            VastausLB.AutoSize = true;
+            VastausLB.Location = new Point(46, 101);
+            VastausLB.Margin = new Padding(5, 0, 5, 0);
+            VastausLB.Name = "VastausLB";
+            VastausLB.Size = new Size(76, 25);
+            VastausLB.TabIndex = 2;
+            VastausLB.Text = "Vastaus";
+            VastausLB.Visible = false;
+            // 
+            // LukujenJarjestys
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(589, 211);
+            Controls.Add(VastausLB);
+            Controls.Add(uusiLukuTB);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(5, 5, 5, 5);
+            Name = "LukujenJarjestys";
+            Text = "Lukujen järjestys";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox uusiLukuTB;
+        private Label VastausLB;
+    }
+}
